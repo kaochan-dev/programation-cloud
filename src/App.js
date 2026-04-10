4ca0f46f8a719cc92fa6274d3a43b66608f83918
kaochan-dev <unitedflaze@gmail.com>
2026-04-10 12:06:11 +0000

    Ajout d'un portfolio React pour démontrer le hook Git

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mon Portfolio</h1>
+        <p>Bienvenue sur mon portfolio React.</p>
+        <p>Ce site démontre le bon fonctionnement du hook Git.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
